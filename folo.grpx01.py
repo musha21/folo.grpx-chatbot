51af227bc500ba1b66b1a3f08511dd207ec65b75
mushastudio <mrmusha2021@gmail.com>
2025-07-22 00:50:34 +0530

    Day 1 - Flask hello world

--- folo.grpx01.py
+++ folo.grpx01.py
@@ -1,1 +1,10 @@
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Folo.grpx!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
